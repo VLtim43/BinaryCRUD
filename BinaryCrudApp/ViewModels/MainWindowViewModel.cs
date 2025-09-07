00d8e8d5207ae0e9424e3ad8328021ee364ed6e0
VLtim43 <fernandoreal.valentim@gmail.com>
2025-09-07 10:22:06 -0300

    initial setup

---
+++ BinaryCrudApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace BinaryCrudApp.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ BinaryCrudApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BinaryCrudApp.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
