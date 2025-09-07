68450d31165e07729b4eedc34b2deefbe2177fb2
VLtim43 <fernandoreal.valentim@gmail.com>
2025-09-07 10:49:09 -0300

    initial setup

--- BinaryCRUD/ViewModels/MainWindowViewModel.cs
+++ BinaryCRUD/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,27 @@
-﻿namespace BinaryCRUD.ViewModels;
+﻿using System.IO;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BinaryCRUD.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty]
+    private string text = string.Empty;
+
+    [RelayCommand]
+    private void Save()
+    {
+        SaveToBinaryFile();
+    }
+
+    private void SaveToBinaryFile()
+    {
+        if (string.IsNullOrEmpty(Text))
+            return;
+
+        byte[] data = Encoding.UTF8.GetBytes(Text);
+        File.WriteAllBytes("data.bin", data);
+    }
 }
